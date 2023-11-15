ddb037caec66067e6b55d3d79c053de6a76804d8
Flor Escobar <e.florescobar@go.ugr.es>
2023-11-15 11:50:19 +0100

    Creación de controlador para cotizador de moneda

---
+++ scambia-api/app/Http/Controllers/ApplicationController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Http\Request;
+use Validator;
+
+class ApplicationController extends Controller
+{
+}
---
+++ scambia-api/app/Http/Controllers/CurrencyRateController.php
@@ -0,0 +1,67 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Validator;
+
+class CurrencyRateController extends Controller
+{
+    public function calculateExchange(Request $request)
+    {
+        try
+        {  
+            $message=''; 
+            $amount = $request->input('amount');
+            $ask = 3.784;
+
+            $validator=Validator::make(
+            [
+                '¿Cuanto envias?' => trim($request->input('amount'))
+            ],
+            [
+                '¿Cuanto envias?' => ['required']
+            ],
+            [
+                'required' => 'El campo ":Attribute" es requerido. '
+            ]);
+
+            if($validator->fails())
+            {
+                $errors=$validator->errors()->all();
+    
+                foreach($errors as $value)
+                {
+                    $message.=$value;
+                }
+                return response()->json(['data' => null, 'message'=>$message, 'errorCode'=>'1']);
+            }
+
+            /*$currencyRate = CurrencyRate::where('idCurrency', $currency)
+                ->where('state', 1)
+                ->first();
+
+            if(count($currencyRate)>0)
+            {
+                return response()->json(['data' => null, 'message'=>'No existe ninguna moneda para el dia '.date("d/m/Y").'!', 'errorCode'=>1]);             
+            }
+
+            */
+
+            $quotedAmount = round( $amount / $ask, 2, PHP_ROUND_HALF_DOWN);
+
+            $data = [
+                "quotedAmount" => $quotedAmount
+            ];    
+
+            return response()->json(['data' => $data , 'message'=>(strlen($message)>0 ? $message : 'Registro guardado correctamente'), 'errorCode'=>(strlen($message)>0 ? 1 : 0)]);
+                
+        }        
+        catch(\Exception $e)
+        {
+            DB::rollback();
+            return response()->json(['data' => null, 'message'=>'Ha ocurrido un error inesperado. Intente nuevamente: '.$e, 'errorCode'=>1]);     
+        }
+    }
+}
